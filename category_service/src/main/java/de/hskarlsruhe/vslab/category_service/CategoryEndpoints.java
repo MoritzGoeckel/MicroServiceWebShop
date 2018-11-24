0f6a4e99447e13f7b4b812f432b179da9f5650a7
Moritz Goeckel <moritz.goeckel@gmail.com>
2018-11-24 09:12:57 +0100

    Added the implementation of the category service

---
+++ category_service/src/main/java/de/hskarlsruhe/vslab/category_service/CategoryEndpoints.java
@@ -0,0 +1,64 @@
+package de.hskarlsruhe.vslab.category_service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
+
+@RestController
+public class CategoryEndpoints {
+
+    @Autowired
+    private CategoryRepo repo;
+
+    @RequestMapping(value = "/categories/", method = RequestMethod.POST)
+    public ResponseEntity<Category> postCategory(@RequestHeader String name) {
+        boolean nameAlreadyExists = StreamSupport
+                .stream(repo.findAll().spliterator(), true)
+                .anyMatch(c -> c.getName().equals(name));
+
+        if(nameAlreadyExists)
+            return new ResponseEntity<>(HttpStatus.ALREADY_REPORTED);
+
+        Category category = new Category(name);
+        category = repo.save(category);
+        return new ResponseEntity<>(category, HttpStatus.OK);
+    }
+
+    @RequestMapping(value = "/categories/", method = RequestMethod.GET)
+    public ResponseEntity<Iterable<Category>> getCategories() {
+        return new ResponseEntity<>(repo.findAll(), HttpStatus.OK);
+    }
+
+    @RequestMapping(value = "/categories/", method = RequestMethod.GET)
+    public ResponseEntity<Iterable<Category>> getCategories(@RequestHeader String name) {
+        Iterable<Category> foundCategories = StreamSupport
+                .stream(repo.findAll().spliterator(), true)
+                .filter(c -> c.getName().contains(name)).collect(Collectors.toList());
+
+        return new ResponseEntity<>(foundCategories, HttpStatus.OK);
+    }
+
+    @RequestMapping(value = "/categories/{id}", method = RequestMethod.GET)
+    public ResponseEntity<Category> getCategoryById(@PathVariable Long id) {
+        Optional<Category> optionalCategory = repo.findById(id);
+
+        return optionalCategory
+                .map(category -> new ResponseEntity<>(category, HttpStatus.OK))
+                .orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
+    }
+
+    @RequestMapping(value = "/categories/{id}", method = RequestMethod.DELETE)
+    public ResponseEntity<Iterable<Category>> deleteCategory(@PathVariable Long id) {
+        if(repo.existsById(id)){
+            repo.deleteById(id);
+            return new ResponseEntity<>(HttpStatus.OK);
+        }
+        else
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+}
---
+++ category_service/src/main/java/de/hskarlsruhe/vslab/category_service/CategoryRepo.java
@@ -0,0 +1,4 @@
+package de.hskarlsruhe.vslab.category_service;
+
+public interface CategoryRepo extends org.springframework.data.repository.CrudRepository<Category, Long> {
+}
