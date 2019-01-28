dac419ad01148d0d6ae422dfae062efe188465df
ChaudhryAhmed <sajeelahmed@outlook.de>
2019-01-28 14:11:43 +0100

    OAuth2 Service added

---
+++ ServiceImplementation/OAuth2/src/main/java/hska/vslab/sec/sprindsecauthserver/OAuth2Application.java
@@ -0,0 +1,14 @@
+package hska.vslab.sec.sprindsecauthserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OAuth2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OAuth2Application.class, args);
+	}
+
+}
+
---
+++ ServiceImplementation/OAuth2/src/test/java/hska/vslab/sec/sprindsecauthserver/SprindSecAuthServerApplicationTests.java
@@ -0,0 +1,17 @@
+package hska.vslab.sec.sprindsecauthserver;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SprindSecAuthServerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
